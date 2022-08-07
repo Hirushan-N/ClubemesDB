167256ab241c6d4e46a1484ed858cf92e11ac40d
Hirushan <nadeeshhirushan@gmail.com>
2022-08-07 18:01:07 +0530

    Add project files.

---
+++ ClubemesDB/dbo/Stored Procedures/Member/SP_Member_Read.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[SP_Member_Read]
+AS
+BEGIN
+        SELECT * FROM [dbo].[tbl_Member] ;
+END
---
+++ ClubemesDB/dbo/Tables/tbl_Member.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbl_Member] (
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [MembershipCode] NVARCHAR (50) NOT NULL,
+    [FirstName]      VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([MembershipCode] ASC)
+);
+
